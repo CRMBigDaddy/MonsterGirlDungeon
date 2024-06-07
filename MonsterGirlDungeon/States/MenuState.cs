16d0aeb5c95afd016fbc6ec7d239159f89bb2602
CRMBigDaddy <107771368+CRMBigDaddy@users.noreply.github.com>
2024-06-07 17:10:49 +0200

    OptinoMenu Implemented

--- MonsterGirlDungeon/States/MenuState.cs
+++ MonsterGirlDungeon/States/MenuState.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,60 +23,17 @@ namespace MonsterGirlDungeon.States
 
         private bool _showOptions = false;
 
-        public event EventHandler changeRes;
+        private Texture2D optionsMenuEdge;
+
+
 
 
         public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, GraphicsDeviceManager graphics) : base(game, graphicsDevice, content)
         {
             _graphics = graphics;
 
-            Texture2D optionButtontexture = _content.Load<Texture2D>("textures/ButtonOptionos");
-            Vector2 optionButtonPos = new Vector2(10, 70);
-
-            Button optionsButton = new Button(optionButtontexture, optionButtonPos);
-            optionsButton.Click += optionsButton_Click;
-
-            _components = new List<Components>()
-            {
-                optionsButton,
-            };
-
-            Texture2D resButtonTexture = _content.Load<Texture2D>("textures/ResolutionButton");
-            Vector2 resButtonPos = new Vector2(10, 10);
-
-            Button resButton = new Button(resButtonTexture, resButtonPos);
-            resButton.Click += ResButton_Click;
-
-            _hiddenComponents = new List<Components>()
-            {
-                resButton,
-            };
-        }
-
-        private void optionsButton_Click(object sender, EventArgs e)
-        {
-            if(!_showOptions)
-            {
-                _showOptions = true;
-            }
-            else if(_showOptions)
-            {
-                _showOptions = false;
-            }
-        }
-
-        private void ResButton_Click(object sender, EventArgs e)
-        {
-            foreach (var component in _components)
-            {
-                component.ChangeScaleFactor(1);
-            }
-
-
-            _graphics.PreferredBackBufferHeight += 180;
-            _graphics.PreferredBackBufferWidth += 320;
-
-            _graphics.ApplyChanges();
+            MainMenu();
+            OptionsMenu();
 
         }
 
@@ -96,6 +54,7 @@ namespace MonsterGirlDungeon.States
                 {
                     component.Draw(gameTime, spriteBatch);
                 }
+
             }
 
             spriteBatch.End();
@@ -107,6 +66,14 @@ namespace MonsterGirlDungeon.States
             {
                 component.Update(gameTime);
             }
+
+            if (_showOptions)
+            {
+                foreach (var component in _hiddenComponents)
+                {
+                    component.Update(gameTime);
+                }
+            }
         }
 
         public override void PostUpdate(GameTime gameTime)
@@ -115,6 +82,87 @@ namespace MonsterGirlDungeon.States
         }
 
         //Logic Method Region
+
+        //Menus Setup
+        #region Menu SetUp
+        private void MainMenu()
+        {
+            Texture2D optionButtontexture = _content.Load<Texture2D>("textures/ButtonOptionos");
+            Vector2 optionButtonPos = new Vector2(10, 70);
+
+            Button optionsButton = new Button(optionButtontexture, optionButtonPos);
+            optionsButton.Click += optionsButton_Click;
+
+            Texture2D exitButtontexture = _content.Load<Texture2D>("textures/ButtonExit");
+            Vector2 exitButtonPos = new Vector2(10, 150);
+
+            Button exitButton = new Button(exitButtontexture, exitButtonPos);
+            exitButton.Click += ExitButton_Click;
+
+            _components = new List<Components>()
+            {
+                optionsButton,
+                exitButton,
+            };
+        }
+
+        private void OptionsMenu()
+        {
+            Texture2D optionWindowTexture = _content.Load<Texture2D>("textures/OptionsWindow");
+            Vector2 optionWindowPos = new Vector2(95, 20);
+
+            MenuWindow optionsWindow = new MenuWindow(optionWindowTexture, optionWindowPos);
+
+            Texture2D resButtonTexture = _content.Load<Texture2D>("textures/ResolutionButton");
+            Vector2 resButtonPos = new Vector2(100, 30);
+
+            Button resButton = new Button(resButtonTexture, resButtonPos);
+            resButton.Click += ResButton_Click;
+
+            _hiddenComponents = new List<Components>()
+            {
+                optionsWindow,
+                resButton,
+            };
+        }
+        #endregion
+        //Button Events
+        #region Button Events
+        private void optionsButton_Click(object sender, EventArgs e)
+        {
+            if (!_showOptions)
+            {
+                _showOptions = true;
+            }
+            else if (_showOptions)
+            {
+                _showOptions = false;
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
+
+        private void ResButton_Click(object sender, EventArgs e)
+        {
+            foreach (var component in _components)
+            {
+                component.ChangeScaleFactor(1);
+            }
+            foreach (var component in _hiddenComponents)
+            {
+                component.ChangeScaleFactor(1);
+            }
+
+            _graphics.PreferredBackBufferHeight += 180;
+            _graphics.PreferredBackBufferWidth += 320;
+
+            _graphics.ApplyChanges();
+
+        }
+        #endregion
 
     }
 }
